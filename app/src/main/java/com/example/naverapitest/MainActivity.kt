97689874c130b555f22c14fa0c4a626a0a6d8515
benchoi <csw284891@naver.com>
2021-05-26 17:52:10 +0900

    첫번쨰 커밋

---
+++ app/src/main/java/com/example/naverapitest/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.naverapitest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val clientId = "n03Xbi3jn2bl6FLFa4p3"
+    val clientSecret = "fog8eyV1JQ"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun searchNews() {
+
+    }
+}
